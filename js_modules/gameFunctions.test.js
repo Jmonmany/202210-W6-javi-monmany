0a9aa4759990ff476df092c6b94f1fd99a832604
Javi <monmany1992@gmail.com>
2022-11-12 17:17:37 +0100

    Added test function to check sonar coverage

--- js_modules/gameFunctions.test.js
+++ js_modules/gameFunctions.test.js
@@ -1,1 +1,32 @@
 //
+import { isArray } from './gameFunctions';
+
+// const arrayTest = [
+//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
+//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
+//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
+//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
+//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
+//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
+//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
+//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
+//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
+// ];
+
+const cases = [6, 'Hello', false, NaN, undefined, null];
+
+describe('Given isArray function', () => {
+    test.each(cases)(
+        `When the input is %p the result should be false`,
+        (value) => {
+            const expected = false;
+            const result = isArray(value);
+            expect(result).toBe(expected);
+        }
+    );
+    test(`When the input is an array the result should be true`, () => {
+        const expected = true;
+        const result = isArray(cases);
+        expect(result).toEqual(expected);
+    });
+});
