f81262e3a74cfea074f0017471e6fae35afa3c7c
Javi <monmany1992@gmail.com>
2022-11-13 20:19:35 +0100

    Added countlive function

--- js_modules/gameFunctions.js
+++ js_modules/gameFunctions.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 //
 export const isArray = (value) => {
     if (value === null) return false;
@@ -22,6 +23,7 @@ function make2DArray(columns, rows) {
 let grid;
 let columns = 10;
 let rows = 10;
+
 function setup() {
     grid = make2DArray(columns, rows);
     for (let i = 0; i < columns; i++) {
@@ -29,9 +31,43 @@ function setup() {
             grid[i][j] = randomNumber(1, 0);
         }
     }
-    console.log(grid)
+    console.log(grid);
 }
-setup()
+
+// function nextArray() {
+//     let next = make2DArray(columns, rows);
+//     for (let i = 0; i < columns; i++) {
+//         for (let j = 0; j < rows; j++) {
+
+//             let state = grid[i][j];
+
+//             if (i === 0 || i === columns - 1 || j === 0 || j === rows - 1) {
+//                 next[i][j] = state;
+//             } else {
+//                 let neighbors = countNeighbors(grid, i, j);
+
+//                 if (state === 0 && neighbors === 3) {
+//                     next[i][j] = 1;
+//                 }
+//                 if( state === 1 && (neighbors < 2 || neighbors > 3)) {
+//                     next[i][j] = 0;
+//                 } else {
+//                     next[i][j] = state;
+//                 }
+//             }
+//         }
+//     }
+// }
+function countNeighbors(grid, x, y) {
+    let sum = 0;
+    for (let i = -1; i < 2; i++) {
+        for (let j = -1; j < 2; j++) {
+            sum += grid[x + i][y + j];
+        }
+    }
+    sum -= grid[x][y];
+    return sum;
+}
 
 // const arrayTest = [
 //     [0, 1, 0],
