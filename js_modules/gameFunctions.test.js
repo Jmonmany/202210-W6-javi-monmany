725391f9e2cfab3e052a7780eb45daafe22eaad6
Javi <monmany1992@gmail.com>
2022-11-13 17:54:22 +0100

    Added randomNumber function and arrayGenerator

--- js_modules/gameFunctions.test.js
+++ js_modules/gameFunctions.test.js
@@ -1,17 +1,8 @@
 //
-import { isArray } from './gameFunctions';
-
-// const arrayTest = [
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     [0, 0, 0, 0, 0, 0, 0, 0, 0],
-// ];
+import {
+    isArray,
+    randomNumber
+} from './gameFunctions';
 
 const cases = [6, 'Hello', false, NaN, undefined, null];
 
@@ -30,3 +21,95 @@ describe('Given isArray function', () => {
         expect(result).toEqual(expected);
     });
 });
+
+describe('Given the randomNumber function', () => {
+    test(``, () => {
+        const result = randomNumber(1000, 1);
+        const expected = result > 1 || result < 1000;
+        expect(expected).toBe(true);
+    });
+});
+
+// describe('Given the generationInterval function', () => {
+//     const arrayTest = [
+//         [0, 0, 0],
+//         [0, 0, 0],
+//         [0, 0, 0],
+//     ];
+//     test(`arrays should be different from each other`, () => {
+//         const expected = arrayTest === generationInterval(arrayTest);
+//         const result = expected === generationInterval(arrayTest);
+//         expect(result).toBe(false);
+//     });
+// });
+
+// describe('Given the checkDeathOrAlive function', () => {
+//     const arrayTest = [
+//         [0, 0, 0, 0, 0, 0, 0, 0, 0],
+//         [0, 0, 0, 0, 0, 0, 0, 0, 0],
+//         [0, 0, 0, 0, 0, 0, 0, 0, 0],
+//         [0, 0, 0, 0, 0, 0, 0, 0, 0],
+//         [0, 0, 0, 0, 0, 0, 0, 0, 0],
+//         [0, 0, 0, 0, 0, 0, 0, 0, 0],
+//         [0, 0, 0, 0, 0, 0, 0, 0, 0],
+//         [0, 0, 0, 0, 0, 0, 0, 0, 0],
+//         [0, 0, 0, 0, 0, 0, 0, 0, 0],
+//     ];
+//     generationCell(arrayTest)
+//     test.each(arrayTest)(`arrays should be different from each other`, (value) => {
+//         const expected = arrayTest
+//         const result = value
+//         expect(result).toBe(expected);
+//     });
+// });
+
+// describe('Given the checkHorizontal function', () => {
+//     const arrayTest = [
+//         [0, 0, 0],
+//         [1, 1, 1],
+//         [0, 0, 0],
+//     ];
+//     test(`the result should be true`, () => {
+//         const expected = true;
+//         const result = checkHorizontal(arrayTest);
+//         expect(result).toBe(expected);
+//     });
+// });
+
+// describe('Given the checkVertical function', () => {
+//     const arrayTest = [
+//         [0, 1, 0],
+//         [0, 1, 0],
+//         [0, 1, 0],
+//     ];
+//     test(`the result should be true`, () => {
+//         const expected = true;
+//         const result = checkVertical(arrayTest);
+//         expect(result).toBe(expected);
+//     });
+// });
+
+// describe('Given the checkDiagonallyUp function', () => {
+//     const arrayTest = [
+//         [0, 0, 1],
+//         [0, 1, 0],
+//         [1, 0, 0],
+//     ];
+//     test(`the result should be true`, () => {
+//         const expected = true;
+//         const result = checkDiagonallyUp(arrayTest[1][1]);
+//         expect(result).toBe(expected);
+//     });
+// });
+// describe('Given the checkDiagonallyDown function', () => {
+//     const arrayTest = [
+//         [1, 0, 0],
+//         [0, 1, 0],
+//         [0, 0, 1],
+//     ];
+//     test(`the result should be true`, () => {
+//         const expected = true;
+//         const result = checkDiagonallyDown(arrayTest[1][1]);
+//         expect(result).toBe(expected);
+//     });
+// });
