097ea8b88804b59659a6c47eabfd308cde6be10a
Javi <monmany1992@gmail.com>
2022-11-14 16:09:44 +0100

    Added non-borders changes

--- js_modules/gameFunctions.test.js
+++ js_modules/gameFunctions.test.js
@@ -1,7 +1,11 @@
 //
 import {
     isArray,
-    randomNumber
+    randomNumber,
+    // setup,
+    // countNeighbors,
+    // nextArray,
+    // make2DArray,
 } from './gameFunctions';
 
 const cases = [6, 'Hello', false, NaN, undefined, null];
@@ -29,87 +33,3 @@ describe('Given the randomNumber function', () => {
         expect(expected).toBe(true);
     });
 });
-
-// describe('Given the generationInterval function', () => {
-//     const arrayTest = [
-//         [0, 0, 0],
-//         [0, 0, 0],
-//         [0, 0, 0],
-//     ];
-//     test(`arrays should be different from each other`, () => {
-//         const expected = arrayTest === generationInterval(arrayTest);
-//         const result = expected === generationInterval(arrayTest);
-//         expect(result).toBe(false);
-//     });
-// });
-
-// describe('Given the checkDeathOrAlive function', () => {
-//     const arrayTest = [
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//         [0, 0, 0, 0, 0, 0, 0, 0, 0],
-//     ];
-//     generationCell(arrayTest)
-//     test.each(arrayTest)(`arrays should be different from each other`, (value) => {
-//         const expected = arrayTest
-//         const result = value
-//         expect(result).toBe(expected);
-//     });
-// });
-
-// describe('Given the checkHorizontal function', () => {
-//     const arrayTest = [
-//         [0, 0, 0],
-//         [1, 1, 1],
-//         [0, 0, 0],
-//     ];
-//     test(`the result should be true`, () => {
-//         const expected = true;
-//         const result = checkHorizontal(arrayTest);
-//         expect(result).toBe(expected);
-//     });
-// });
-
-// describe('Given the checkVertical function', () => {
-//     const arrayTest = [
-//         [0, 1, 0],
-//         [0, 1, 0],
-//         [0, 1, 0],
-//     ];
-//     test(`the result should be true`, () => {
-//         const expected = true;
-//         const result = checkVertical(arrayTest);
-//         expect(result).toBe(expected);
-//     });
-// });
-
-// describe('Given the checkDiagonallyUp function', () => {
-//     const arrayTest = [
-//         [0, 0, 1],
-//         [0, 1, 0],
-//         [1, 0, 0],
-//     ];
-//     test(`the result should be true`, () => {
-//         const expected = true;
-//         const result = checkDiagonallyUp(arrayTest[1][1]);
-//         expect(result).toBe(expected);
-//     });
-// });
-// describe('Given the checkDiagonallyDown function', () => {
-//     const arrayTest = [
-//         [1, 0, 0],
-//         [0, 1, 0],
-//         [0, 0, 1],
-//     ];
-//     test(`the result should be true`, () => {
-//         const expected = true;
-//         const result = checkDiagonallyDown(arrayTest[1][1]);
-//         expect(result).toBe(expected);
-//     });
-// });
